f1d06682c9c484dda39dad91644b19da7b09ca95
Paul Kienzle <paul.kienzle@nist.gov>
2012-04-16 10:27:27 -0400

    fully qualified module path example

--- modpath/main.py
+++ modpath/main.py
@@ -1,8 +1,11 @@
+import sys,os
 from .subpackage import submod
 
 def main():
     print "From main, use path.to.module.fn.__module__",submod.fn.__module__
     submod.fn()
+    print "package directory for submod",os.path.dirname(sys.modules[submod.__name__].__file__)
+    print "full path package directory for submod",os.path.abspath(os.path.dirname(sys.modules[submod.__name__].__file__))
 
 if __name__ == "__main__":
     main()
---
+++ modpath/subpackage/__init__.py
@@ -0,0 +1,1 @@
+print "subpackage __name__",__name__
